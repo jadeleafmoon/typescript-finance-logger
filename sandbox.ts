f12bc790a5b6dbaccf0acd72f7c97d5c467de205
jadeleafmoon <jadeleafmoon@gmail.com>
2023-05-14 17:41:34 -0400

    feat: test arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,36 @@
-let character = "mario";
-let age = 30;
-let isBlackBelt = false;
+let names = ['luigi', 'mario', 'luigi'];
 
-// character = 20;
-character = "luigi";
-age = 40;
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-}
+let nums = [10, 20, 30, 40];
+// nums.push('hi');
+// nums[13] = "hey";
+nums.push(50);
 
-console.log(circ("10"));
+// arrays
+let mixed = ["bob", 10, "enzo", 5, false];
+mixed.push("dot");
+mixed.push(50);
+mixed.push(true);
+// mixed.push(undefined);
+mixed[0] = 3;
+
+//objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30,
+};
+
+ninja.age = 40;
+// ninja.name = 3;
+// ninja.skills = "fighting";
+
+ninja = {
+  name: "luigi",
+  belt: "red",
+  age: 40,
+  // skills: "fight"
+};
