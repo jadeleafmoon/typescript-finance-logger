5ed1cce8d0e52c36d7d28964b6ba6706427d1896
jadeleafmoon <jadeleafmoon@gmail.com>
2023-05-19 18:58:01 -0400

    feat: implement ListTemplate

--- public/app.js
+++ public/app.js
@@ -1,26 +1,6 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-// // we guarantee that everything pushed into docs has the 'format()' method
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 350);
-let invoices = [];
-// invoices.push("hello"); X
-invoices.push(invOne);
-invoices.push(invTwo);
-// we can change properties
-invOne.client = "yoshi";
-invTwo.amount = 400;
-invoices.forEach(inv => {
-    console.log(inv.client, inv.details, inv.amount, inv.format());
-});
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
@@ -28,6 +8,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -37,5 +20,6 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
+    list.render(doc, type.value, 'end');
     console.log(doc);
 });
