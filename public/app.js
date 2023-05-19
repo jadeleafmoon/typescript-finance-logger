709ed41ee39b826e421a6ff2b9d4e932fc6b709d
jadeleafmoon <jadeleafmoon@gmail.com>
2023-05-19 19:49:47 -0400

    refactor: cleanup code

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@ import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
-// console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -22,30 +21,3 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
-// Generics
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-// let docOne = addUID({name: 'yoshi', age: 40});
-// console.log("docOne", docOne.age);
-// ENUMS
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
-})(ResourceType || (ResourceType = {}));
-const docOne = {
-    uid: 1,
-    resourceName: ResourceType.BOOK,
-    data: { title: 'Lord of the Rings' }
-};
-const docTwo = {
-    uid: 10,
-    resourceName: ResourceType.PERSON,
-    data: { name: 'yoshi' }
-};
-console.log(docOne, docTwo);
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import { HasFormatter } from './interfaces/HasFormatter.js';
 import { ListTemplate } from './classes/ListTemplate.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -29,41 +28,3 @@ form.addEventListener('submit', (e: Event) => {
   list.render(doc, type.value, 'end');
 
 });
-
-// Generics
-
-const addUID = <T extends {name: string}>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return {...obj, uid};
-}
-
-// let docOne = addUID({name: 'yoshi', age: 40});
-
-// console.log("docOne", docOne.age);
-
-// ENUMS
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
-//           index:  0,    1,      2,    3,        4
-
-
-interface Resource<T> {
-  uid: number;
-  resourceName: ResourceType;
-  data: T;
-}
-
-const docOne: Resource<object> = {
-  uid: 1,
-  resourceName: ResourceType.BOOK,
-  data: { title: 'Lord of the Rings' }
-}
-
-const docTwo: Resource<object> = {
-  uid: 10,
-  resourceName: ResourceType.PERSON,
-  data: { name: 'yoshi' }
-}
-
-console.log(docOne, docTwo);
-// {uid: 1, resourceName: 0, data: {…}} 
-// {uid: 10, resourceName: 4, data: {…}}
